0c618366a66dd821b719f08cafa83e35c5b200d3
Mahzan <110054003+mahzan123@users.noreply.github.com>
2023-10-18 19:44:59 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 const rootElement = document.querySelector(":root");
+const song = document.querySelector('#song');
+const audioIcon = document.querySelector('.audio-icon');
+let isPlaying = false;
+
+
 
 function disableScroll() {
   scrollTop = window.pageYOffset || document.documentElement.scrollTop;
@@ -14,11 +19,27 @@ function disableScroll() {
 function enableScroll() {
   window.onscroll = function() {}
   rootElement.style.scrollBehavior = 'smooth';
-  localStorage.setItem('opened', 'true');
+  //localStorage.setItem('opened', 'true');
+  audioPlay();
+}
+
+function audioPlay() {
+  song.volume = 0.3;
+  audioIcon.style.display = "flex";
+  song.play();
+}
+
+audioIcon.onclick = function() {
+  if(isPlaying) {
+    song.pause();
+  } else {
+    song.play();
+  }
+  isPlaying = !isPlaying;
 }
 
 
-if(!localStorage.getItem('opened')) {
+//if(!localStorage.getItem('opened')) {
   disableScroll();
-}
+//}
 
